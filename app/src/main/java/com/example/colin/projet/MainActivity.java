2c0e0eed2f344b71be28fc8c1d0f1788643b362e
JZJeff <jeff.zufferey@students.hevs.ch>
2017-04-04 10:49:14 +0200

    add test var

--- app/src/main/java/com/example/colin/projet/MainActivity.java
+++ app/src/main/java/com/example/colin/projet/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int nombre = 0;
+    String blabla ="test";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
