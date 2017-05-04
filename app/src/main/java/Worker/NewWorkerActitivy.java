cd34f8134c2bffe62331edb126dce9ec0b12cae7
colinchappot <colin.chappot@students.hevs.ch>
2017-05-04 18:19:58 +0200

    Version ok

--- app/src/main/java/Worker/NewWorkerActitivy.java
+++ app/src/main/java/Worker/NewWorkerActitivy.java
@@ -1,29 +1,41 @@
 package Worker;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.colin.projet.R;
+
+import DB.DbHelper;
+import DB.FeedReaderContract;
 
 public class NewWorkerActitivy extends AppCompatActivity {
 
     private EditText etxtFirstname;
     private EditText etxtLastname;
     private EditText etxtCellphone;
+    private EditText etxtLogin;
+    private EditText etxtPassword;
+
     private Button btnsaveWorker;
 
+    String idWorker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_worker_actitivy);
 
+        Intent intent = getIntent();
+        idWorker = intent.getStringExtra("idWorker");
 
-
+        etxtLogin = (EditText) findViewById(R.id.eTxtLogin);
+        etxtPassword = (EditText) findViewById(R.id.eTxtPassword);
         etxtFirstname = (EditText) findViewById(R.id.eTxtFirstname);
         etxtLastname = (EditText) findViewById(R.id.eTxtLastname);
         etxtCellphone = (EditText) findViewById(R.id.eTxtCellphone);
@@ -31,10 +43,21 @@ public class NewWorkerActitivy extends AppCompatActivity {
         btnsaveWorker.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
                 Intent intent=new Intent(NewWorkerActitivy.this, WorkerListMenuActivity.class);
+                intent.putExtra("idWorker",idWorker);
                 startActivity(intent);
                 finish();
             }
         });
+    }
+    public void add()
+    {
+        DbHelper db = new DbHelper(this);
+
+        db.InsertWorker(this,etxtLogin.getText().toString(), etxtPassword.getText().toString(),etxtFirstname.getText().toString(),etxtLastname.getText().toString(),etxtCellphone.getText().toString());
+
+        Toast.makeText(getApplicationContext(), this.getString(R.string.newWorkerCreated), Toast.LENGTH_SHORT).show();
+
     }
 }
