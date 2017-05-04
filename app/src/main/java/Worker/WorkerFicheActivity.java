3a26a18edf9337b7dfed1b9cef889a911b3ee317
JZJeff <jeff.zufferey@students.hevs.ch>
2017-05-04 17:29:58 +0200

    Téléphone oki ;)

--- app/src/main/java/Worker/WorkerFicheActivity.java
+++ app/src/main/java/Worker/WorkerFicheActivity.java
@@ -1,23 +1,23 @@
 package Worker;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.colin.projet.LoginActivity;
 import com.example.colin.projet.R;
-
-import java.util.ArrayList;
 
 import DB.DbHelper;
 import DB.FeedReaderContract;
@@ -32,10 +32,10 @@ public class WorkerFicheActivity extends AppCompatActivity {
     private TextView txtBCellPhone;
     private TextView txtVCellPhone;
     private Button btnCall;
-    private Button btnMessage;
     private ImageView imgWorker;
 
     String idWorker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,27 +45,23 @@ public class WorkerFicheActivity extends AppCompatActivity {
         idWorker = intent.getStringExtra("idWorker");
         Toast.makeText(getApplicationContext(), idWorker, Toast.LENGTH_SHORT).show();
 
-        String message="";
+        String message = "";
 
 
         btnCall = (Button) findViewById(R.id.btnCall);
-        btnMessage = (Button) findViewById(R.id.btnMessage);
         imgWorker = (ImageView) findViewById((R.id.imgWorker));
 
 
-
-        SQLiteDatabase dbR= new DbHelper(this).getReadableDatabase();
-
+        SQLiteDatabase dbR = new DbHelper(this).getReadableDatabase();
 
 
-
-        Cursor c = dbR.rawQuery("SELECT * FROM " + FeedReaderContract.Worker.TABLE_NAME+" where "+FeedReaderContract.Worker._ID+" = "+idWorker, null);
+        final Cursor c = dbR.rawQuery("SELECT * FROM " + FeedReaderContract.Worker.TABLE_NAME + " where " + FeedReaderContract.Worker._ID + " = " + idWorker, null);
         Resources res = getResources();
 
 
         if (c.moveToFirst()) {
             message = c.getString(3);
-            TextView  textView = (TextView) findViewById(R.id.txtVFirstname);
+            TextView textView = (TextView) findViewById(R.id.txtVFirstname);
             textView.setText(message);
             message = c.getString(4);
             textView = (TextView) findViewById(R.id.txtVLastname);
@@ -74,14 +70,25 @@ public class WorkerFicheActivity extends AppCompatActivity {
             textView = (TextView) findViewById(R.id.txtVCellphone);
             textView.setText(message);
 
-           int resID = res.getIdentifier("worker"+c.getString(0), "drawable",  WorkerFicheActivity.this.getPackageName());
+            int resID = res.getIdentifier("worker" + c.getString(0), "drawable", WorkerFicheActivity.this.getPackageName());
 
             ImageView imageView = (ImageView) findViewById(R.id.imgWorker);
             imageView.setImageResource(resID);
         }
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + c.getString(5)));
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
+
     }
-
-
 
 
 }
