a14e538f5bb9b0e3394d6e3a6377ba251e16c03e
JZJeff <jeff.zufferey@students.hevs.ch>
2017-05-04 19:38:24 +0200

    MAJFrame

--- app/src/main/java/Playground/PlaygroundFicheActivity.java
+++ app/src/main/java/Playground/PlaygroundFicheActivity.java
@@ -58,7 +58,7 @@ public class PlaygroundFicheActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+
         btnSave = (Button) findViewById(R.id.btnAddTask);
 /*        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
