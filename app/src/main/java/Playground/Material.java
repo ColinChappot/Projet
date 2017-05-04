c2f22a2bbea64e3e10c58a4d2a2f4e7d770b8572
JZJeff <jeff.zufferey@students.hevs.ch>
2017-05-04 11:43:11 +0200

    CouleurThem changer Tools/android/themeEditor

---
+++ app/src/main/java/Playground/Material.java
@@ -0,0 +1,18 @@
+package Playground;
+
+/**
+ * Created by uadmin on 04.05.2017.
+ */
+
+public class Material {
+
+    private String materialName;
+
+    public Material(String materialName){
+        this.materialName=materialName;
+    }
+
+    public String getMaterialName(){
+        return this.materialName;
+    }
+}
